047e17991443f4ce3217ef661b667da1696c2cdb
dpcivl <dpcivl713@gmail.com>
2024-11-14 17:27:01 +0900

    :sparkles: 미세먼지 도메인 클래스

--- s/ngsd/src/main/java/in/parkhyo/ngsd/model/ParticleMatter.java
+++ s/ngsd/src/main/java/in/parkhyo/ngsd/model/ParticleMatter.java
@@ -1,5 +1,19 @@
 package in.parkhyo.ngsd.model;
 
+import java.time.LocalDateTime;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
 public class ParticleMatter {
 
+    private String location;
+    private LocalDateTime timestamp;
+    private Double pm10;
+    private Double pm2_5;
+    private Double pm1_0;
 }
